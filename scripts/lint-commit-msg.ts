5f8c6435e16242c26b5607d11ecbf8f1236f3041
abitwhy <736490428@qq.com>
2022-06-06 17:33:47 +0800

    chore: update lint-commit-msg

--- scripts/lint-commit-msg.ts
+++ scripts/lint-commit-msg.ts
@@ -2,29 +2,19 @@
 // origin from https://github.com/vitejs/vite/blob/main/scripts/verifyCommit.ts
 
 import { readFileSync } from 'fs'
-import colors from 'picocolors'
 
 // get $1 from commit-msg script
 const msgPath = process.argv[2]
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
-const releaseRE = /^v\d/
 const commitRE =
   /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
 
-if (!releaseRE.test(msg) && !commitRE.test(msg)) {
+if (!commitRE.test(msg)) {
   /* eslint-disable no-console */
-  console.log()
-  console.error(
-    `  ${colors.bgRed(colors.white(' ERROR '))} ${colors.red(
-      `invalid commit message format.`
-    )}\n\n` +
-      colors.red(
-        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
-      ) +
-      `    ${colors.green(`feat: add 'comments' option`)}\n` +
-      `    ${colors.green(`fix: handle events on blur (close #28)`)}\n\n` +
-      colors.red(`  See .github/commit-convention.md for more details.\n`)
+  console.log(
+    `[${new Date().toLocaleString()}] > ERROR:`,
+    `'${msg}' is a invalid commit message format!`
   )
   process.exit(1)
 }
