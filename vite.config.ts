907b5b4e1f9d64f1268003652e9482b361287157
abitwhy <736490428@qq.com>
2022-06-10 09:47:25 +0800

    chore: router

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,10 @@ export default defineConfig({
       extensions: ['vue', 'md'],
       include: [/\.vue$/, /\.vue\?vue/],
       dts: 'src/components/index.d.ts',
+    }),
+    Pages({
+      extensions: ['vue'],
+      exclude: ['**/-/**'],
     }),
   ],
 })
