a9b8a52309412682f9decfa3bfd34033d884f1ab
abitwhy <736490428@qq.com>
2022-06-21 18:05:29 +0800

    test: improve sample of component

--- src/components/TodoList/.test.ts
+++ src/components/TodoList/.test.ts
@@ -1,9 +1,31 @@
 import { mount } from '@vue/test-utils'
 import TodoList from './index.vue'
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const wrapper = mount(TodoList)
+
 test('renders a todo', () => {
-  const wrapper = mount(TodoList)
   const todo = wrapper.get('[data-test="todo"]')
   expect(todo.text()).toBe('Learn Vue.js 3')
   expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('creates a todo', async () => {
+  await wrapper.get('[data-test="new-todo"]').setValue('New todo')
+  await wrapper.get('[data-test="form"]').trigger('submit')
+
+  expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+})
+
+test('usage of get: find a tag', () => {
+  expect(wrapper.get('input').element.tagName).toBe('INPUT')
+})
+
+test('usage of find + exists: find a not exist tag', () => {
+  expect(wrapper.find('.not-exist').exists()).toBe(false)
+})
+
+test(`usage of isVisible: assert tag's visibility`, () => {
+  expect(wrapper.find('[data-test="visible"]').isVisible()).toBe(false)
 })
--- src/components/TodoList/index.vue
+++ src/components/TodoList/index.vue
@@ -1,17 +1,59 @@
 <template>
-  <div>
-    <div v-for="todo in todos" :key="todo.id" data-test="todo">
+  <form data-test="form" @submit.prevent="createTodo">
+    <input v-model="newTodo" data-test="new-todo" />
+    <button type="submit" :disabled="!newTodo">提交</button>
+    <div v-show="false" data-test="visible"></div>
+  </form>
+  <ol>
+    <li v-for="todo in todos" :key="todo.id" data-test="todo">
       {{ todo.text }}
-    </div>
-  </div>
+    </li>
+  </ol>
 </template>
 
 <script setup lang="ts">
-const todos = [
+import { reactive } from 'vue'
+
+type Todo = {
+  id: number
+  text: string
+  completed: boolean
+}
+withDefaults(
+  defineProps<{
+    todos?: Todo[]
+  }>(),
   {
-    id: 0,
-    text: 'Learn Vue.js 3',
-    completed: false,
+    todos: () =>
+      reactive([
+        {
+          id: 0,
+          text: 'Learn Vue.js 3',
+          completed: false,
+        },
+      ]),
+  }
+)
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      newTodo: '',
+    }
   },
-]
+  methods: {
+    createTodo() {
+      this.todos.push({
+        id: this.todos.length + 1,
+        text: this.newTodo,
+        completed: false,
+      })
+      this.newTodo = ''
+    },
+  },
+})
 </script>
