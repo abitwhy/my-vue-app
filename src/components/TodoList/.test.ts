160b1f92d842a1a8c7eb097c9514422597dee344
abitwhy <736490428@qq.com>
2022-07-21 17:15:14 +0800

    feat: remove and check

--- src/components/TodoList/.test.ts
+++ src/components/TodoList/.test.ts
@@ -1,31 +1,77 @@
 import { mount } from '@vue/test-utils'
 import TodoList from './index.vue'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+// 静态测试可共用一个组件实例
 const wrapper = mount(TodoList)
 
-test('renders a todo', () => {
-  const todo = wrapper.get('[data-test="todo"]')
-  expect(todo.text()).toBe('Learn Vue.js 3')
-  expect(wrapper.html()).toMatchSnapshot()
-})
-
-test('creates a todo', async () => {
-  await wrapper.get('[data-test="new-todo"]').setValue('New todo')
-  await wrapper.get('[data-test="form"]').trigger('submit')
-
-  expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
-})
-
-test('usage of get: find a tag', () => {
+//#region 基础语法
+test('usage of get: get a exist tag', () => {
   expect(wrapper.get('input').element.tagName).toBe('INPUT')
 })
 
-test('usage of find + exists: find a not exist tag', () => {
+test('usage of find + exists: find a may not exist tag', () => {
   expect(wrapper.find('.not-exist').exists()).toBe(false)
 })
 
 test(`usage of isVisible: assert tag's visibility`, () => {
   expect(wrapper.find('[data-test="visible"]').isVisible()).toBe(false)
 })
+//#endregion
+
+//#region 真实需求
+test('render', () => {
+  expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('add a todo', async () => {
+  const wrapper = mount(TodoList),
+    { length } = wrapper.findAll('[data-test="todo"]')
+  const text = String(Date.now())
+
+  await wrapper.get('[data-test="new-todo"]').setValue(text)
+  await wrapper.get('[data-test="form"]').trigger('submit')
+
+  expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(length + 1)
+  expect(wrapper.get('[data-test="todo"]:last-child').text()).toBe(text)
+  expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+})
+
+test('remove a todo', async () => {
+  const wrapper = mount(TodoList),
+    { length } = wrapper.findAll('[data-test="todo"]'),
+    randomnNth = randomIntIn(1, length),
+    todo = wrapper.find(`[data-test="todo"]:nth-child(${randomnNth})`)
+
+  await todo.find('button').trigger('click')
+
+  expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(length - 1)
+  expect(
+    wrapper
+      .find(
+        `[data-test="todo"]:nth-child(${randomnNth}):nth-last-child(${
+          length - randomnNth + 1
+        })`
+      )
+      .exists()
+  ).toBe(false)
+  expect(wrapper.emitted()).toHaveProperty('remove-todo')
+  expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+})
+
+test('check a todo', async () => {
+  const wrapper = mount(TodoList),
+    { length } = wrapper.findAll('[data-test="todo"]'),
+    randomnNth = randomIntIn(1, length),
+    todoInput = wrapper.find(
+      `[data-test="todo"]:nth-child(${randomnNth}) input`
+    )
+
+  await todoInput.trigger('change')
+
+  expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+})
+//#endregion
+
+function randomIntIn(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
--- src/components/TodoList/index.vue
+++ src/components/TodoList/index.vue
@@ -1,59 +1,91 @@
 <template>
-  <form data-test="form" @submit.prevent="createTodo">
-    <input v-model="newTodo" data-test="new-todo" />
+  <form data-test="form" @submit.prevent="add">
+    <input v-model.trim="newTodo" data-test="new-todo" />
     <button type="submit" :disabled="!newTodo">提交</button>
     <div v-show="false" data-test="visible"></div>
   </form>
   <ol>
-    <li v-for="todo in todos" :key="todo.id" data-test="todo">
-      {{ todo.text }}
+    <li v-for="(todo, i) in todos" :key="i" class="todo" data-test="todo">
+      <label v-for="({ text, checked }, i) of [todo]" :key="i">
+        <input type="checkbox" :checked="checked" @change="toggle(todo)" />
+        {{ text }}
+      </label>
+      <button type="button" @click="remove(i)"></button>
     </li>
   </ol>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
-
-type Todo = {
-  id: number
-  text: string
-  completed: boolean
-}
 withDefaults(
   defineProps<{
-    todos?: Todo[]
+    modelValue?: Todo[]
   }>(),
   {
-    todos: () =>
+    modelValue: () =>
       reactive([
         {
           id: 0,
-          text: 'Learn Vue.js 3',
-          completed: false,
+          text: 'Learn VueJS 3',
+          checked: false,
         },
       ]),
   }
 )
+
+defineEmits<{
+  (e: 'update:modelValue', todos: Todo[]): void
+  (e: 'remove-todo', todo: Todo, index: number): void
+}>()
 </script>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+
+type Todo = {
+  id: number
+  text: string
+  checked: boolean
+}
 
 export default defineComponent({
-  data() {
+  data: ({ modelValue }): { newTodo: string; todos: Todo[] } => {
     return {
       newTodo: '',
+      todos: modelValue,
     }
   },
+  watch: {
+    todos: {
+      handler(todos) {
+        this.$emit('update:modelValue', todos)
+      },
+      deep: true,
+    },
+  },
   methods: {
-    createTodo() {
+    add() {
       this.todos.push({
         id: this.todos.length + 1,
         text: this.newTodo,
-        completed: false,
+        checked: false,
       })
       this.newTodo = ''
+    },
+    remove(index: number) {
+      const [todo] = this.todos.splice(index, 1)
+      this.$emit('remove-todo', todo, index)
+    },
+    toggle(target: Todo) {
+      target.checked = !target.checked
     },
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.todo {
+  > button::before {
+    content: 'x';
+  }
+}
+</style>
