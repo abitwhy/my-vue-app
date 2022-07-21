77d3c373193e6f01aea7f07d9ac1598262990645
abitwhy <736490428@qq.com>
2022-07-21 10:19:58 +0800

    perf(lint): turn rule off
    vue/no-template-shadow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,12 +28,22 @@ module.exports = defineConfig({
     /*
     多词组件名
     规则：[vue/multi-word-component-names](https://eslint.vuejs.org/rules/multi-word-component-names.html})
-    个人觉得上述规则制定理由并不充分，理由如下：
+    理由：
     0. 如果 Vue.js 在编译组件时，不进行单词组件名的大小写转化，即：<Component /> != <component />，那么可能引发的冲突将迎刃而解。
     1. 普通组件名与内置组件名或原生标签名的冲突：普通组件名遵守大写首字母即可避免。
     2. 路由组件名可以保持小写也不会引发上述冲突，因为路由组件不会出现在 template 中。
     */
     'vue/multi-word-component-names': 'off',
+
+    /*
+    模板局部同名变量
+    规则：[vue/no-template-shadow](https://eslint.vuejs.org/rules/no-template-shadow.html)
+    理由：
+    1. 如果此规则是出于语法考虑（模板中不完全支持局部作用域），那么应当尽快完善该语法编译器；
+      如果是出于阅读考虑，那完全没必要，这种重复且通用的变量名就是为了避免不必要的具象。
+    2. 命名的成本很高
+    */
+    'vue/no-template-shadow': 'off',
 
     // typescript
     '@typescript-eslint/no-unused-vars': 'error',
