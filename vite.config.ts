9e4333a2ab0bb24a504d27a89327c4a8f5f0fdbb
abitwhy <736490428@qq.com>
2022-07-26 18:15:35 +0800

    chore: remove unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,14 +11,7 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [
-    vue(),
-    Components({
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/],
-      dts: 'src/components/index.d.ts',
-    }),
-  ],
+  plugins: [vue()],
   test: {
     globals: true,
     environment: 'jsdom',
