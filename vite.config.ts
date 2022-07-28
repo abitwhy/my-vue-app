240d77056393caa55f90e03e3808b9da5d4e1508
abitwhy <736490428@qq.com>
2022-07-28 17:41:37 +0800

    refactor: remove dependency of node.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
-import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+      '~/': '/src/',
     },
   },
   plugins: [vue()],
